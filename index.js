8f63832a72fa79f64018d8013ff8186f2d18a428
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-05 11:11:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("hello there");
+
+class AnimalStack {
+  constructor() {
+    this.stack = [];
+  }
+  get length() {
+    return this.stack.length;
+  }
+  push(item) {
+    return this.stack.push(item);
+  }
+  pop() {
+    return this.stack.pop();
+  }
+  peek() {
+    return this.stack[this.length - 1];
+  }
+}
+
+const stack = new AnimalStack();
+stack.push("fox");
+stack.push("goose");
+stack.push("lizard");
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push("llama");
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
